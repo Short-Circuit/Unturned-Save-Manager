7fed31180c6c16c3cc4d9413fdfe394a87c0442f
Short-Circuit <P_Ir_At_Es@comcast.net>
2014-10-30 16:04:40 -0700

    Added map naming

--- src/com/shortcircuit/unturnedsavemanager/UnturnedSaveManager.java
+++ src/com/shortcircuit/unturnedsavemanager/UnturnedSaveManager.java
@@ -9,10 +9,14 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
@@ -20,13 +24,14 @@ import javax.swing.border.EtchedBorder;
 
 import com.shortcircuit.unturnedsavemanager.managers.FileWatchThread;
 import com.shortcircuit.unturnedsavemanager.managers.RegistrySaveManager;
+import com.shortcircuit.unturnedsavemanager.registry.WinRegistry;
 
 /**
  * @author ShortCircuit908
  */
 public class UnturnedSaveManager extends JFrame{
     private static final long serialVersionUID = -1182579377443890522L;
-    public final String version = "1.1.0";
+    public final String version = "1.1.1";
     private JTextField name_text;
     public JList<String> save_list;
     private Thread search_thread;
@@ -39,6 +44,7 @@ public class UnturnedSaveManager extends JFrame{
     private JButton btn_delete;
     private JButton btn_reset;
     private JCheckBox export_players;
+    private JLabel current_map;
     /**
      * Launch the application.
      */
@@ -57,6 +63,7 @@ public class UnturnedSaveManager extends JFrame{
      * Initialize the contents of the frame.
      */
     private void initialize() {
+        saveDefaults();
         reg_manager = new RegistrySaveManager();
         setResizable(false);
         setTitle("Unturned Save Manager v" + version);
@@ -65,7 +72,7 @@ public class UnturnedSaveManager extends JFrame{
         setBounds(100, 100, 450, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
-        
+
         btn_select = new JButton("Select save");
         btn_select.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
@@ -86,7 +93,7 @@ public class UnturnedSaveManager extends JFrame{
         btn_select.setEnabled(false);
         btn_select.setBounds(10, 238, 201, 23);
         getContentPane().add(btn_select);
-        
+
         save_list = new JList<String>();
         save_list.addMouseListener(new MouseAdapter() {
             @Override
@@ -101,7 +108,7 @@ public class UnturnedSaveManager extends JFrame{
         });
         save_list.setValueIsAdjusting(true);
         save_list.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-        save_list.setBounds(10, 11, 201, 216);
+        save_list.setBounds(10, 35, 201, 192);
         getContentPane().add(save_list);
 
         btn_load = new JButton("Load");
@@ -109,6 +116,7 @@ public class UnturnedSaveManager extends JFrame{
             public void actionPerformed(ActionEvent event) {
                 if(!txt_save.getText().equals("Please select a save...")) {
                     reg_manager.importReg(txt_save.getText());
+                    refreshCurrentMap();
                 }
             }
         });
@@ -180,31 +188,65 @@ public class UnturnedSaveManager extends JFrame{
             public void actionPerformed(ActionEvent event) {
                 if(!name_text.getText().equals("Please enter a name...")){
                     reg_manager.save(name_text.getText(), export_players.isSelected());
+                    refreshCurrentMap();
                 }
             }
         });
         btn_confirm.setEnabled(false);
         btn_confirm.setBounds(338, 134, 96, 23);
         getContentPane().add(btn_confirm);
-        
+
         btn_reset = new JButton("Reset current map");
         btn_reset.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 reg_manager.resetMap();
+                refreshCurrentMap();
             }
         });
         btn_reset.setBounds(233, 10, 201, 23);
         getContentPane().add(btn_reset);
-        
+
         export_players = new JCheckBox("Include player data");
         export_players.setToolTipText("Save player data (inventory, location, skills, etc)");
         export_players.setSelected(true);
         export_players.setEnabled(false);
         export_players.setBounds(231, 108, 203, 23);
         getContentPane().add(export_players);
-        
+
+        current_map = new JLabel("Current map:");
+        current_map.setBounds(10, 10, 201, 14);
+        getContentPane().add(current_map);
+
         search_thread = new Thread(new FileWatchThread(this));
         search_thread.start();
         setVisible(true);
+        refreshCurrentMap();
+    }
+    public void refreshCurrentMap() {
+        try {
+            String map_name = WinRegistry.readString(WinRegistry.HKEY_CURRENT_USER, "Software\\Smartly "
+                    + "Dressed Games\\Unturned", "MapName");
+            if(map_name == null) {
+                map_name = "Unnamed map";
+            }
+            current_map.setText("Current map: " + map_name);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void saveDefaults() {
+        try {
+            File saves = new File("Saves");
+            saves.mkdirs();
+            String[] files = {"LICENSE", "README", "CHANGELOG", "USAGE"};
+            for(String file_name : files) {
+                Files.copy(UnturnedSaveManager.class.getResourceAsStream("/" + file_name + ".txt"),
+                        new File(file_name + ".txt").toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/com/shortcircuit/unturnedsavemanager/managers/RegistrySaveManager.java
+++ src/com/shortcircuit/unturnedsavemanager/managers/RegistrySaveManager.java
@@ -23,18 +23,24 @@ public class RegistrySaveManager {
     }
     public void save(String name, boolean save_players) {
         try{
-            String data = "Windows Registry Editor Version 5.00\n\n[HKEY_CURRENT_USER\\Software\\Smartly Dressed Games\\Unturned]\n";
-            String key_string = "barricades structures vehicles";
+            String data = "Windows Registry Editor Version 5.00\n\n[HKEY_CURRENT_USER\\Software\\Smartly "
+                    + "Dressed Games\\Unturned]\n";
+            String key_string = "MapName barricades structures vehicles";
             if(save_players) {
                 key_string += " inventory last clothes skills life position";
             }
             String[] keys = key_string.split(" ");
             for(String key : keys) {
                 List<String> reg_keys = getMatchingKeys(key);
-                for(String reg_key : reg_keys) {
-                    data += "\"" + reg_key + "\"=\"" + WinRegistry.readString(WinRegistry.HKEY_CURRENT_USER,
-                            "Software\\Smartly Dressed Games\\Unturned", reg_key) + "\"\n";
-                    
+                if(reg_keys.isEmpty() && key.equals("MapName")) {
+                    data += "\"MapName\"=\"" + name + "\"\n";
+                }
+                else {
+                    for(String reg_key : reg_keys) {
+                        String reg_entry = WinRegistry.readString(WinRegistry.HKEY_CURRENT_USER,
+                                "Software\\Smartly Dressed Games\\Unturned", reg_key);
+                        data += "\"" + reg_key + "\"=\"" + reg_entry + "\"\n";
+                    }
                 }
             }
             composeReg(name, data);
@@ -71,8 +77,8 @@ public class RegistrySaveManager {
         }
     }
     public void resetMap() {
-        String[] keys = {"barricades", "structures", "vehicles", "inventory", "last", "clothes", "skills",
-                "life", "position"};
+        String[] keys = {"MapName", "barricades", "structures", "vehicles", "inventory", "last", "clothes",
+                "skills", "life", "position"};
         for(String key : keys) {
             List<String> reg_keys = getMatchingKeys(key);
             for(String reg_key : reg_keys) {
@@ -101,10 +107,11 @@ public class RegistrySaveManager {
                     results.add(line.split("    ")[0]);
                 }
             }
+            proc.waitFor();
             scan.close();
             proc.destroy();
         }
-        catch(IOException e) {
+        catch(Exception e) {
             e.printStackTrace();
         }
         return results;
